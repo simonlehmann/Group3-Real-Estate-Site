70678acd5b576d6dd59e06357b249a1ea9b02713
ulternate <contact@danielcswain.com>
2016-04-10 15:36:21 +0800

    Working on add/edit layouts, added some fake data to the edit action for layout testing.

--- app/controllers/sell_controller.rb
+++ app/controllers/sell_controller.rb
@@ -59,7 +59,21 @@ class SellController < ApplicationController
 	# Show the add/edit form ready for user input to edit an existing property
 	# GET /sell/:id/edit
 	def edit
-		# handle edits
+		# For testing, I'm passing fake property data
+		@property = {
+			id: 1,
+			address: "21 Shackles Street, Suburbia, Stateland",
+			status: "Home open Saturday 10am - 12pm",
+			images: ["300x300.png","300x300.png","300x300.png","300x300.png","300x300.png","300x300.png","300x300.png","300x300.png","300x300.png","300x300.png"],
+			description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+			counts: {
+				unhide: [450, "Views"],
+				star: [30, "Favourites"],
+				comments: [5, "Comments"],
+				wait: [6, "Weeks Until Expired"]
+			}
+		}
+		
 		render "sell/add_edit"
 	end
 
