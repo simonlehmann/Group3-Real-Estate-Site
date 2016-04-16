130ffda42e9513cd32a5aea700bd99d98876aede
ulternate <contact@danielcswain.com>
2016-04-16 23:45:40 +0800

    Adding user model, updated db/schema.rb to reflect actual database state, using random user object in sell_controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160413053732) do
+ActiveRecord::Schema.define(version: 0) do
 
   create_table "blockages", primary_key: "BlockageID", force: :cascade do |t|
     t.integer  "BlockageToUserID",   limit: 4, null: false
@@ -85,3 +85,18 @@ ActiveRecord::Schema.define(version: 20160413053732) do
     t.integer  "TagListingID", limit: 4,  null: false
     t.datetime "TagCreatedAt",            null: false
   end
+
+  create_table "users", primary_key: "UserID", force: :cascade do |t|
+    t.string   "UserUsername",     limit: 32,                     null: false
+    t.string   "UserFirstName",    limit: 32,                     null: false
+    t.string   "UserMiddleName",   limit: 32
+    t.string   "UserLastName",     limit: 32,                     null: false
+    t.string   "UserEmailAddress", limit: 128,                    null: false
+    t.string   "UserPassword",     limit: 64,                     null: false
+    t.string   "UserType",         limit: 0,   default: "User",   null: false
+    t.string   "UserStatus",       limit: 0,   default: "Active", null: false
+    t.datetime "UserCreatedAt",                                   null: false
+    t.datetime "UserUpdatedAt",                                   null: false
+  end
+
+end
