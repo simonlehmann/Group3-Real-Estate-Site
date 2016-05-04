2bf73e91af5aecbafb67dd29d6c2972dad0951a4
Michael <michael.white308@gmail.com>
2016-05-04 15:44:48 +0800

    Autocomplete for searching suburbs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,20 @@ module ApplicationHelper
 	def devise_mapping
 		@devise_mapping ||= Devise.mappings[:user]
 	end
+
+	# The following allow us to use the location database
+	def get_suburbs (state = "Western Australia")
+		return @states = Location.where(state: state).sort!
+	end
+	def get_postcodes (state = "Western Australia")
+		return @postcodes = Location.where(state: state).sort!
+	end
+	def get_suburbs_postcodes (state = "Western Australia")
+		objects = Location.where(state: state)
+		@suburbs_postcodes = []
+		objects.each do |object|
+			@suburbs_postcodes << "#{object.suburb}, #{object.postcode}"
+		end
+		return @suburbs_postcodes.sort!
+	end
 end
