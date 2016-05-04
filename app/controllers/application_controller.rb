7107b3681bea21abdc7bc1af73211a3548197a31
Simon Lehmann <slehmann36@gmail.com>
2016-05-04 15:13:02 +0800

    Added desplaying of user's actual information in navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,14 @@ class ApplicationController < ActionController::Base
 	# Prevent CSRF attacks by raising an exception.
 	# For APIs, you may want to use :null_session instead.
 	protect_from_forgery with: :exception
-	
+
 	# Required to redirect back to previous page on successfull sign-in/out/up
 	after_filter :store_action
+	before_filter :get_user_details
 
 	# Store the previous url if it wasn't one of the sign-in/out/up ones (used by devise to redirect after completion of the devise action)
 	def store_action
-		return unless request.get? 
+		return unless request.get?
 		if (request.path != "/login" &&
 			request.path != "/sign_up" &&
 			request.path != "/password/new" &&
@@ -32,5 +33,15 @@ class ApplicationController < ActionController::Base
 		requesting_route_controller = Rails.application.routes.recognize_path(request.referrer)[:controller]
 		# Go back to the refering request on logout unless it was a dashboard site (as the user has logged out) in which case go to root_path.
 		requesting_route_controller != "dashboard" ? request.referrer : root_path
+	end
+end
+
+def get_user_details
+	user = current_user
+	if user
+		@username = user.username
+		@first_name = user.first_name
+		@last_name = user.last_name
+		@email = user.email
 	end
 end
