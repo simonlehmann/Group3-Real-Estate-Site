00aa739407b2b572aa4ccff705227ecc8dc6e373
ulternate <contact@danielcswain.com>
2016-05-06 21:45:03 +0800

    Adding live preview to the avatar change in dashboard settings. Added replacement file picker button that prompts the file picker. Added dashboard colors and classes to colors.sass and dashboard.sass respectively

--- app/models/user.rb
+++ app/models/user.rb
@@ -83,8 +83,8 @@ class User < ActiveRecord::Base
 
 	# User avatar
 	has_attached_file :avatar, styles: {
-		thumb: "100x100>",
-		medium: "300x300>"
+		thumb: "100x100#",
+		medium: "300x300#"
 	}, :default_url => "/avatars/:style/missing.png"
 	validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
