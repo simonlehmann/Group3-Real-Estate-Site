6a393122160748570898a9af1d439d89f17b4441
Michael <michael.white308@gmail.com>
2016-05-18 12:47:23 +0800

    Format twitter profile image url method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -275,28 +275,4 @@ module ApplicationHelper
 		# Return a sorted list
 		return @return_array.sort!
 	end
-
-	# Method to turn links in a tweet into a clickable link instead of plain text
-	def parsed_tweet tweet
-		_parsed_tweet = tweet.text.dup
-
-		# Turn URL's into clickable links
-		tweet.urls.each do |entity|
-			html_link = link_to(entity.display_url.to_s, entity.expanded_url.to_s, target: '_blank')
-			_parsed_tweet.sub!(entity.url.to_s, html_link)
-		end
-		# Turn media links into clickable links
-		tweet.media.each do |entity|
-			html_link = link_to(entity.display_url.to_s, entity.expanded_url.to_s, target: '_blank')
-			_parsed_tweet.sub!(entity.url.to_s, html_link)
-		end
-
-		_parsed_tweet.html_safe
-	end
-
-	# Make twitter date human and UX friendly
-	def convert_twitter_date date
-		formatted_date = date.strftime('%d %b, %Y')
-		return formatted_date
-	end
 end
