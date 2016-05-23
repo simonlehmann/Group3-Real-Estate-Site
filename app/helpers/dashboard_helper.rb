90b2c73d177f188f1e7018b8f158f600b9a1e1a6
doritobandito <michael.white308@gmail.com>
2016-05-23 03:56:06 -0700

    Moved to controller from helper

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,11 +1,2 @@
 module DashboardHelper
-	def get_favourites_object_array
-		user = current_user
-		favourites = Favourite.where(favourite_user_id: current_user)
-		return favourites
-	end
-	def get_listing_details(fav_listing_id)
-		listing = Listing.where(listing_id: fav_listing_id)
-		return listing
-	end
 end
