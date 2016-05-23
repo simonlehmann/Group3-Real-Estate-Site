d522dc9e55403006de79df75512c3326f606d884
doritobandito <michael.white308@gmail.com>
2016-05-23 04:06:59 -0700

    Returns one listing, added if for the case of no favourites, moved back to helper

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -51,13 +51,8 @@ class DashboardController < ApplicationController
 	# 	@user = current_user
 	# 	render "dashboard/crop"
 	# end
-	def get_favourites_object_array
-		user = current_user
-		favourites = Favourite.where(favourite_user_id: current_user)
-		return favourites
-	end
 	def get_listing_details(fav_listing_id)
-		listing = Listing.where(listing_id: fav_listing_id)
+		listing = Listing.find_by_listing_id(fav_listing_id)
 		return listing
 	end
 end
