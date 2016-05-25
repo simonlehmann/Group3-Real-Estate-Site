3b8d17d985e69ed902646cf381defafd48fd9f0e
Michael <michael.white308@gmail.com>
2016-05-25 10:32:49 +0800

    Commenterino

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,4 +1,5 @@
 module DashboardHelper
+	# Returns an object array for the users favourites
 	def get_favourites_object_array
 		user = current_user
 		favourites = Favourite.where(favourite_user_id: current_user)
