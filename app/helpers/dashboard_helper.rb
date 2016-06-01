c2d688f3dd6a8057bb5d48ab4632851f30ec53d8
doritobandito <michael.white308@gmail.com>
2016-06-01 04:24:05 -0700

    Finished home open summary

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -13,8 +13,19 @@ module DashboardHelper
 		return status_start_time
 	end
 
+	def get_end_time (listing)
+		status_object = ListingStatus.find(listing.listing_status_id)
+		status_start_time = Time.parse(status_object.listing_status_end_time.to_s).strftime("%H:%M")
+		return status_start_time
+	end
+
 	def get_time_ago (status_update_time)
 		time_distance = time_ago_in_words(status_update_time)
 		return time_distance + " ago"
+	end
+
+	def convert_status_date (date)
+		formatted_date = date.strftime('%B %d, %Y')
+		return formatted_date
 	end
 end
