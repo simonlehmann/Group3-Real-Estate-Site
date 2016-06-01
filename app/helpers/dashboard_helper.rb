c1908d5fa26af45c9bea5b701ef35465d94bdc12
Michael <michael.white308@gmail.com>
2016-06-01 15:15:54 +0800

    Added first phase of activity feed

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -3,6 +3,13 @@ module DashboardHelper
 	def get_favourites_object_array
 		user = current_user
 		favourites = Favourite.where(favourite_user_id: current_user)
+		favourites.order("updated_at")
 		return favourites
+	end
+
+	def get_start_time (listing)
+		status_object = ListingStatus.find(listing.listing_status_id)
+		status_start_time = Time.parse(status_object.listing_status_start_time.to_s).strftime("%H:%M")
+		return status_start_time
 	end
 end
